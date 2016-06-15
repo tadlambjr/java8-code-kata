ab629d0c1cfaf0b91d26fdca90b8de3005dbd85a
tadlambjr <tad.lamb@iq-ity.com>
2016-06-15 13:04:13 -0400

    exercise 1 streams

--- stream-api/src/test/java/stream/api/Exercise1Test.java
+++ stream-api/src/test/java/stream/api/Exercise1Test.java
@@ -1,21 +1,17 @@
 package stream.api;
 
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
 import common.test.tool.annotation.Necessity;
 import common.test.tool.dataset.ClassicOnlineStore;
 import common.test.tool.entity.Customer;
 import common.test.tool.util.AssertUtil;
-
 import org.junit.Test;
 
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.*;
+import java.util.function.*;
+import java.util.stream.*;
 
 public class Exercise1Test extends ClassicOnlineStore {
 
@@ -28,8 +24,8 @@ public class Exercise1Test extends ClassicOnlineStore {
          * Create a {@link Stream} from customerList only including customer who has more budget than 10000.
          * Use lambda expression for Predicate and {@link Stream#filter} for filtering.
          */
-        Predicate<Customer> richCustomerCondition = null;
-        Stream<Customer> richCustomerStream = null;
+        Predicate<Customer> richCustomerCondition = customer -> customer.getBudget() > 10000;
+        Stream<Customer> richCustomerStream = customerList.parallelStream().filter(richCustomerCondition);
 
         assertTrue("Solution for Predicate should be lambda expression", AssertUtil.isLambda(richCustomerCondition));
         List<Customer> richCustomer = richCustomerStream.collect(Collectors.toList());
@@ -47,8 +43,8 @@ public class Exercise1Test extends ClassicOnlineStore {
          * Use method reference(best) or lambda expression(okay) for creating {@link Function} which will
          * convert {@link Customer} to {@link Integer}, and then apply it by using {@link Stream#map}.
          */
-        Function<Customer, Integer> getAgeFunction = null;
-        Stream<Integer> ageStream = null;
+        Function<Customer, Integer> getAgeFunction = Customer::getAge;
+        Stream<Integer> ageStream = customerList.parallelStream().map(getAgeFunction);
 
         assertTrue(AssertUtil.isLambda(getAgeFunction));
         List<Integer> richCustomer = ageStream.collect(Collectors.toList());
